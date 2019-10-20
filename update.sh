b807a475414298e0bd4dcd982cf18df20b93ae4b
coreylam <coreylam@163.com>
2019-10-20 23:16:57 +0800

    add new files

---
+++ update.sh
@@ -0,0 +1,10 @@
+echo "git pull"
+git pull git@github.com:coreylam/mynote
+echo "done"
+echo "add all files"
+git add ./
+echo "git commit"
+git commit -m "add new files"
+echo "git push"
+git push git@github.com:coreylam/mynote
+echo "done"
