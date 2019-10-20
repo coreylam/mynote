066863a33d3f048031f64e4e405b7e7e6a8a3e44
coreylam <coreylam@163.com>
2019-10-20 23:17:50 +0800

    update

--- git_pull.sh
+++ git_pull.sh
@@ -1,5 +1,2 @@
 
 
-echo "git pull"
-git pull git@github.com:coreylam/mynote
-echo "done"
--- git_push.sh
+++
@@ -1,7 +0,0 @@
-echo "add all files"
-git add ./
-echo "git commit"
-git commit -m "add new files"
-echo "git push"
-git push git@github.com:coreylam/mynote
-echo "done"
